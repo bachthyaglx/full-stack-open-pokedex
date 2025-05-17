52d5d58ed079edb9ad6564c0f3b1fdd7e116e938
Thy Khuu <62774638+bachthyaglx@users.noreply.github.com>
2025-05-17 03:55:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable linebreak-style */
-
+import React from 'react'
 import { BrowserRouter as Routes, Route, useMatch } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
