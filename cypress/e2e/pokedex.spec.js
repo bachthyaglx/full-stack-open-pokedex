a8f2b02fa7420c56f14783ff7548b5482c5f8150
Thy Khuu <62774638+bachthyaglx@users.noreply.github.com>
2025-05-17 03:30:52 +0200

    Create pokedex.spec.js

---
+++ cypress/e2e/pokedex.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* global cy */
+
+describe('Pokedex', () => {
+  it('front page can be opened', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be navigated to', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
